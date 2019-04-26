66a0474a1386adefad8d0198b99ce067b79e0b85
Медведева Мария Александр <medvedevama@lab227.local>
2019-04-26 19:10:55 +0300

    Lab 3

--- medvedevama.cpp
+++ medvedevama.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 #include <locale>
 
-double * gauss(double **A, double *b, int N)
+double * Gauss(double **A, double *b, int N)
 {
   double *x, max;
   int k, index;
@@ -32,8 +32,10 @@ double * gauss(double **A, double *b, int N)
     for (int i = 0; i < N; i++)
     {
     change[i]=index;
-    if(index!=i) {swap(A[i],A[index]);
-                  swap(b[i],b[index]); }
+    if(index!=i) {
+		swap(A[i],A[index]);
+        swap(b[i],b[index]);
+		gity}
 
     for (int j = 0; j < N; j++)
     {
@@ -66,9 +68,14 @@ double * gauss(double **A, double *b, int N)
     for (int i = 0; i < k; i++)
       b[i] = b[i] - A[i][k] * x[k];
   }
-   for (int i = N-1; i >= 0; i--) { if (index!=i) {L = change[i];
-                                              swap(A[L], A[i]);
-                                              swap(b[L], b[i]); } }
+   for (int i = N-1; i >= 0; i--)
+    {
+       if (index!=i)
+	      { L = change[i];
+            swap(A[L], A[i]);
+            swap(b[L], b[i]);
+			}
+	}
   return x;
 }
 }
@@ -90,7 +97,7 @@ void medvedevama::lab2()
 {
  system("chcp 1251");
   system("cls");
-  x = gauss(A, b, N);
+  x = Gauss(A, b, N);
   }
 
 
@@ -100,7 +107,38 @@ void medvedevama::lab2()
  */
 void medvedevama::lab3()
 {
+  	double *upper, *middle, *lower; // верхняя, главная и нижняя диагонали
+	upper = new double[N];
+	middle = new double[N];
+	lower = new double[N];
+	double k;
 
+	lower[0] = 0;
+	upper[N-1] = 0;
+
+	for (int i = 0; i < N; i++)//Заполнение"диагональных" массивов
+	{
+		if (i - 1 >= 0 && i - 1 < N)
+		upper[i] = A[i-1][i];
+		middle[i] = A[i][i];
+		if (i + 1 >= 0 && i + 1 < N)
+		lower[i] = A[i+1][i]; //нижняя
+	}
+	//Прямой ход
+	for (int i = 1; i < N; i++) //Вычисляем коэффициенты прогонки
+	{
+		k = lower[i]/middle[i-1];
+		middle[i] = middle[i] - k*upper[i-1];
+		b[i] = b[i] - k*b[i-1];
+	}
+
+	//Обратный ход
+	x[N-1] = b[N-1]/middle[N-1]; //Вычисляется решение
+
+	for (int i = N - 2; i >= 0; i--) //рекуррентная формула для вычисления остальных неизвестных
+		x[i]=(b[i]-upper[i]*x[i+1])/middle[i];
+
+	delete[] upper, middle, lower;
 }
 
 
@@ -110,7 +148,39 @@ void medvedevama::lab3()
  */
 void medvedevama::lab4()
 {
+  	double mis=1e-15;//порядок ошибки
+	double error;
+	double *nx = new double[N];//для хранения промежуточных значений
 
+	for (int i=0;i<N;i++)//для первичного приближения возьмём столбец свободных членов
+		x[i]=b[i];
+	int step=0;
+
+	do
+	{
+		step++;
+	for(int i=0;i < N;i++)
+	{
+		nx[i]=-b[i];
+		for(int j=0;j < N;j++)
+		{
+			if(i!=j)
+			nx[i]+=A[i][j]*x[j];
+		}
+		nx[i]/=-A[i][i];
+	}
+	error=0;
+	for(int i=0; i<N; i++)
+	{
+		if(std::abs(x[i]-nx[i]) > error)//Максимальная разница между элементами решения
+			error = std::abs(x[i]-nx[i]);
+	}
+	for(int i=0; i<N; i++)
+		x[i]=nx[i];
+	std::cout<<step<<"    "<<error<<endl;
+	}
+	while (error>mis);
+	delete[] nx;
 }
 
 
