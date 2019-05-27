272dccfdf5d160385c3ac72fb4962f2fed5df83c
KobyzevKirill <46112272+KobyzevKirill@users.noreply.github.com>
2019-05-27 09:20:12 +0300

    lab 8

--- kobyzevkv.cpp
+++ kobyzevkv.cpp
@@ -306,10 +306,59 @@ void kobyzevkv::lab7()
 	delete[] z;
 }
 
-
+/*
+* Метод вращения для нахождения собственных значений матрицы
+*/
 void kobyzevkv::lab8()
 {
+	double **H = new double*[N], eps = 1.e-10;
+	for (int i = 0; i < N; i++) H[i] = new double[N];
 
+	do
+	{
+		double n = 0;
+		int i_max = 0, j_max = 1;
+		for (int i = 0; i < N; i++)
+			for (int j = i + 1; j < N; j++)
+			{
+				if (fabs(A[i][j]) > fabs(A[i_max][j_max]))
+				{
+					i_max = i;
+					j_max = j;
+				}
+
+				n += A[i][j] * A[i][j];
+			}
+
+		if (sqrt(n) < eps) break;
+
+		double fi = 0.5 * atan(2 * A[i_max][j_max] / (A[i_max][i_max] - A[j_max][j_max]));
+		for (int i = 0; i < N; i++)
+		{
+			H[i][i_max] = A[i][i_max] * cos(fi) + A[i][j_max] * sin(fi);
+			H[i][j_max] = A[i][j_max] * cos(fi) - A[i][i_max] * sin(fi);
+		}
+
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
+				if (j != i_max && j != j_max) H[i][j] = A[i][j];
+
+		for (int j = 0; j < N; j++)
+		{
+			A[i_max][j] = H[i_max][j] * cos(fi) + H[j_max][j] * sin(fi);
+			A[j_max][j] = H[j_max][j] * cos(fi) - H[i_max][j] * sin(fi);
+		}
+
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
+				if (i != i_max && i != j_max) A[i][j] = H[i][j];
+
+	} while (true);
+
+	for (int i = 0; i < N; i++) x[i] = A[i][i];
+
+	for (int i = 0; i < N; i++) delete[] H[i];
+	delete[] H;
 }
 
 /*
